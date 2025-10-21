ced4d1959cdf438d9c9af073593447ebb979ec38
J0Money <joachimh270@gmail.com>
2025-10-21 11:13:15 +0200

    M4.04: Tilføj Cosmos service+model+pages, opdater NavMenu/Program, fjern demo-sider

---
+++ Models/SupportMessage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace SupportWebApp.Models;
+
+public class SupportMessage
+{
+    public string id { get; set; } = Guid.NewGuid().ToString();
+
+    [Required]
+    public string name { get; set; }
+
+    [Required]
+    public string email { get; set; }
+
+    [Required]
+    public string category { get; set; }
+
+    [Required]
+    public string subject { get; set; }
+
+    [Required]
+    public string message { get; set; }
+
+    public DateTime createdAt { get; set; } = DateTime.UtcNow;
+}
---
+++ Services/CosmosSupportService.cs
@@ -0,0 +1,45 @@
+﻿using Azure.Identity;
+using Microsoft.Azure.Cosmos;
+using SupportWebApp.Models;
+
+namespace SupportWebApp.Services;
+
+
+    public class CosmosSupportService
+    {
+        private readonly Container _container;
+
+        public CosmosSupportService(IConfiguration config)
+        {
+            var connectionString = config["Cosmos:ConnectionString"];
+            var databaseName = "IBasSupportDB";
+            var containerName = "ibassupport";
+
+            var client = new CosmosClient(connectionString, new CosmosClientOptions
+            {
+                ApplicationName = "SupportWebApp"
+            });
+
+            _container = client.GetContainer(databaseName, containerName);
+        }
+
+        public async Task AddMessageAsync(SupportMessage message)
+        {
+            await _container.CreateItemAsync(message, new PartitionKey(message.category));
+        }
+
+        public async Task<List<SupportMessage>> GetAllMessagesAsync()
+        {
+            var query = _container.GetItemQueryIterator<SupportMessage>(
+                new QueryDefinition("SELECT * FROM c"));
+
+            var results = new List<SupportMessage>();
+            while (query.HasMoreResults)
+            {
+                FeedResponse<SupportMessage> response = await query.ReadNextAsync();
+                results.AddRange(response.Resource);
+            }
+
+            return results.OrderByDescending(m => m.createdAt).ToList();
+        }
+    }
